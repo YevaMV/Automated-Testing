4a552b1098f48851f0a0878dc09e54f56e82dbb1
YevaMV <eva280988@gmail.com>
2022-03-04 16:21:53 +0400

    writing Integration test

--- util.test.js
+++ util.test.js
@@ -1,6 +1,13 @@
-const { generateText } = require('./util'); //node.js
+const { generateText, checkAndGenerate } = require('./util'); //node.js
 
 test('should output name and age', () => {
   const text = generateText('Max', 30);
+  expect(text).toBe('Max (30 years old)');
+});
+
+// Integration test
+
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Max', 30);
   expect(text).toBe('Max (30 years old)');
 });
